935291d3be68faf72d5c984d40d87912c9886768
zwoou <zwooou@gmail.com>
2018-01-10 19:43:53 +0800

    1.0

--- yhh/src/main/java/bid/woou/YhhApplication.java
+++ yhh/src/main/java/bid/woou/YhhApplication.java
@@ -3,12 +3,15 @@ package bid.woou;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@EnableTransactionManagement
+@ServletComponentScan
 @SpringBootApplication
-@MapperScan("bid.woou.*.dao,bid.woou.common.singleTable.*.mapper")
+@MapperScan("bid.woou.**.mapper")
 public class YhhApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(YhhApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(YhhApplication.class, args);
+    }
 }
